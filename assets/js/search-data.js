701b7379afc015b647c9f6164204ee51344a7e14
tislam93 <80611333+tislam93@users.noreply.github.com>
2026-05-07 22:43:37 +0000

    Deploying to gh-pages from @ tislam93/tislam93.github.io@3eac9b2f2933652cfbb507d06b1f57e9667c2c92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -175,13 +175,6 @@ ninja.data = [{
           window.open("https://gitlab.com/tislam93", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
